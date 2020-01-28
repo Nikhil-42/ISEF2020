50c4d3651460e74b80e4af31cb030a003e8008f4
Nikhil Iyer <iyer.h.nikhil@gmail.com>
2020-01-28 10:26:40 -0500

    implement mutation

--- homebrew/network.py
+++ homebrew/network.py
@@ -3,7 +3,7 @@ import random
 import numpy as np
 import numba
 from numba import jitclass, jit, njit
-from numba import int32, int64, float32, uint8
+from numba import int32, int64, float32, float64, uint8
 import timeit
 
 import time
@@ -22,7 +22,7 @@ def gen_repeated_range(start, stop, repeats, step=1, collate=False):
                 yield n    
 
 @njit
-def relu(x: float32) -> float32:
+def relu(x: float64) -> float64:
     return x if x > 0.0 else 0.0
 
 @njit
@@ -30,11 +30,11 @@ def leaky_relu(x):
     return x if x > 0.0 else x * 0.01
 
 @njit
-def sigmoid(x: float32) -> float32:
+def sigmoid(x: float64) -> float64:
     return 1/(1+math.exp(-x))
 
 @njit
-def d_relu(x: float32) -> float32:
+def d_relu(x: float64) -> float64:
     return 1.0 if x > 0.0 else 0.0
 
 @njit
@@ -42,31 +42,32 @@ def d_leaky_relu(x):
     return 1 if x > 0.0 else 0.01
 
 @njit
-def d_sigmoid(x: float32) -> float32:
+def d_sigmoid(x: float64) -> float64:
     return 1/(1+math.exp(-x))
 
 network_spec = [
     ('input_shape', int32),
     ('output_shape', int32),
     ('node_count', int32),
-    ('nodes', float32[:,:]),
+    ('nodes', float64[:,:]),
     ('to', int32[:,:]),
     ('frm', int32[:,:]),
     ('deps', int32[:,:]),
-    ('weights', float32[:,:]),
+    ('weights', float64[:,:]),
     ('connections', numba.typeof({(int32(0), int32(0))})),
+    ('learning_rate', float32)
 ]
 
 @jitclass(network_spec)
 class JIT_Network:
 
-    def __init__(self, input_shape: int, output_shape: int, node_count=0):
+    def __init__(self, input_shape: int, output_shape: int, node_count=0, learning_rate=0.01):
         np.random.seed(12)
         random.seed(12)
         self.input_shape = input_shape
         self.output_shape = output_shape
         self.node_count = input_shape + output_shape if node_count<input_shape+output_shape else node_count
-        self.nodes = np.empty(shape=(self.node_count, 3), dtype=float32)
+        self.nodes = np.empty(shape=(self.node_count, 3), dtype=float64)
         self.nodes.fill(-1.0)
         self.to = np.empty(shape=(self.node_count, self.node_count), dtype=int32)
         self.frm = np.empty(shape=(self.node_count, self.node_count), dtype=int32)
@@ -74,11 +75,12 @@ class JIT_Network:
             self.to[i, -1] = 0
             self.frm[i, -1] = 0
         self.deps = np.column_stack((np.zeros(shape=self.node_count, dtype=int32), np.full(shape=self.node_count, fill_value=self.node_count, dtype=int32)))
-        self.weights = np.random.random((self.node_count, self.node_count)).astype(float32)
+        self.weights = np.random.random((self.node_count, self.node_count)).astype(float64)/self.node_count
         self.connections = {(int32(0), int32(0))}
+        self.learning_rate = learning_rate
         print("Initialized a JIT_Network: [Input: ", self.input_shape, ", Output: ", self.output_shape, ", Node Count :", self.node_count, "]")
 
-    def get_activation(self, node_i: int32) -> float32:
+    def get_activation(self, node_i: int32) -> float64:
         p_activation = 0.0
 
         for from_node_i in self.frm[node_i][:self.frm[node_i, self.node_count-1]]:
@@ -89,7 +91,7 @@ class JIT_Network:
 
         return self.nodes[node_i, 0]
     
-    def get_backward_delta(self, node_i: int32) -> float32:
+    def get_backward_delta(self, node_i: int32) -> float64:
         p_error = 0.0
 
         for to_node_i in self.to[node_i][:self.to[node_i, self.node_count-1]]:
@@ -122,24 +124,23 @@ class JIT_Network:
         for node_i in range(self.node_count-self.output_shape-1, -1, -1):
             self.get_backward_delta(node_i)
 
-    def update_weights(self, learning_rate):
+    def update_weights(self):
         for connection in self.connections:
-            self.weights[connection] += learning_rate * self.nodes[connection[0], 1] * self.nodes[connection[1], 0]
+            self.weights[connection] += self.learning_rate * self.nodes[connection[0], 1] * self.nodes[connection[1], 0]
         for node_i in range(self.node_count):
-            self.nodes[node_i, 2] += learning_rate * self.nodes[node_i, 1]
+            self.nodes[node_i, 2] += self.learning_rate * self.nodes[node_i, 1]
 
     def predict(self, inputs):
-        outputs = np.zeros((len(inputs), self.output_shape), dtype=float32)
+        outputs = np.zeros((len(inputs), self.output_shape), dtype=float64)
         for i in numba.prange(len(inputs)):
             outputs[i] = self.forward_propagate(inputs[i])
         return outputs
 
-    def validate(self, val_x, val_y, post_processing):
+    def validate(self, val_x, val_y, compare):
         correct = 0
         for i in range(len(val_x)):
             output_layer = self.forward_propagate(val_x[i])
-            processed_layer = np.array([post_processing(e) for e in output_layer], dtype=val_y.dtype)
-            if np.array_equal(processed_layer, val_y[i]):
+            if compare(output_layer, val_y[i]):
                 correct += 1
             else:
                 # print(output_layer, "=/=", val_y[i])
@@ -157,7 +158,7 @@ class JIT_Network:
         for i in range(self.node_count):
             self.nodes[i, 2] = 0.0
 
-    def train(self, x: list, y: list, n_epoch=1, learning_rate=0.01, batch_size=1000, target_error=0.01):
+    def train(self, x: list, y: list, n_epoch=1, batch_size=1000, target_error=0.01):
 
         # # Generate forward layers
         # layer = [0]
@@ -204,8 +205,8 @@ class JIT_Network:
                 for index in range(batch_size):
                     i = b + index
                     output_layer = self.forward_propagate(x[i])
-                    self.backward_propagate_error(np.array([(y[i, j] - output_layer[j]) * d_leaky_relu(output_layer[j]) for j in range(self.output_shape)], dtype=float32))
-                    self.update_weights(learning_rate)
+                    self.backward_propagate_error(np.array([(y[i, j] - output_layer[j]) * d_leaky_relu(output_layer[j]) for j in range(self.output_shape)], dtype=float64))
+                    self.update_weights()
                     for j in range(self.output_shape):
                         error += (y[i, j] - output_layer[j])**2
 
@@ -239,25 +240,6 @@ class JIT_Network:
         self.deps[frm, 1] = min((to, self.deps[frm, 1]))
         self.connections.add((int32(to), int32(frm)))
 
-    def set_connections(self, connections):
-        self.to[:,-1].fill(0)
-        self.frm[:,-1].fill(0)
-        self.connections = connections
-        
-        for connection in connections:
-            to, frm = connection
-            if to <= frm:
-                self.connections.remove(connection)
-                continue
-            # print("connecting node[", frm, "] to node[", to, "]")
-            self.to[frm, self.to[frm, self.node_count-1]] = to
-            self.to[frm, self.node_count-1] += 1
-            self.deps[to, 0] = max((self.deps[to, 0], frm))
-            self.frm[to, self.frm[to, self.node_count-1]] = frm
-            self.frm[to, self.node_count-1] += 1
-            self.deps[frm, 1] = min((to, self.deps[frm, 1]))
-            # print("connected  node[", frm, "] to node[", to, "]")
-
     def add_connections(self, tos, frms):
         new_connections = set(map(lambda x, y: (x, y), tos, frms))
         new_connections.difference_update(self.connections)
@@ -277,7 +259,34 @@ class JIT_Network:
 
         self.connections.update(new_connections)
 
-                
+    def set_connections(self, connections):
+        self.to[:,-1].fill(0)
+        self.frm[:,-1].fill(0)
+        self.connections = connections
+        
+        for connection in connections:
+            to, frm = connection
+            if to <= frm:
+                self.connections.remove(connection)
+                continue
+            # print("connecting node[", frm, "] to node[", to, "]")
+            self.to[frm, self.to[frm, self.node_count-1]] = to
+            self.to[frm, self.node_count-1] += 1
+            self.deps[to, 0] = max((self.deps[to, 0], frm))
+            self.frm[to, self.frm[to, self.node_count-1]] = frm
+            self.frm[to, self.node_count-1] += 1
+            self.deps[frm, 1] = min((to, self.deps[frm, 1]))
+            # print("connected  node[", frm, "] to node[", to, "]")
+    
+    def remove_connection(self, connection):
+        if connection in self.connections:
+            self.connections.remove(connection)   
+        self.set_connections(self.connections)
+
+    def remove_random_connection(self):
+        if len(self.connections > 1):
+            self.remove_connection(self.connections.pop())
+
 def run_xor_test():
     np.random.seed(0)
     random.seed(0)
@@ -285,19 +294,24 @@ def run_xor_test():
     x = np.array([[1, 0] if rand < 0.25 else [0, 1] if rand < 0.5 else [1, 1] if rand < 0.75 else [0,0] for rand in np.random.random(60000)], dtype=int)
     y = np.array([[1,] if np.sum(itm)==1 else [0,] for itm in x], dtype=int)
 
-    model = JIT_Network(input_shape=2, output_shape=1, node_count=5)
-    # for i in range(2, 102):
+    node_count = 5
+
+    model = JIT_Network(input_shape=2, output_shape=1, node_count=node_count, learning_rate=0.1)
+    # for i in range(2, node_count-1):
     #     model.add_connection(i, 0)
     #     model.add_connection(i, 1)
-    #     model.add_connection(102, i)
+    #     model.add_connection(node_count-1, i)
 
     model.add_connections(np.array([2,2,3,3,4,4]), np.array([0, 1, 0, 1, 2, 3]))
     
     @njit
-    def nopython_round(x):
-        return round(x)
+    def nopython_round(output_layer, expected):
+        truth = 1
+        for i, out in enumerate(output_layer):
+            truth += not round(out) == expected[i]
+        return truth == 1
 
-    print("Training error: ", model.train(x, y, 1, 0.1, 1000, 0.001))
+    print("Training error: ", model.train(x, y, 1, 1000, 0.001))
     print("Validation accuracy: ", model.validate(x, y, nopython_round))
     # model.prefit()
     print(model.predict(((1, 1), (1, 0), (0, 1), (0, 0))))
